9146a271aae21efea7ce99179694014f2bd3592d
Oleg <lvgelo@mail.ru>
2020-07-10 11:25:23 +0500

    +artifacts +gitignore +code format +command line processing

--- src/ru/ovk/home/Main.java
+++ src/ru/ovk/home/Main.java
@@ -1,9 +1,33 @@
 package ru.ovk.home;
 
+import java.util.Arrays;
+import static ru.ovk.home.constant.TerminalConst.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        DisplayTerminalMess(args);
     }
-    
+
+
+    private static void DisplayTerminalMess(final String[] args){
+        String sCommand="";
+
+        if(args.length<1 || args==null)  sCommand = "default";
+        else  sCommand=args[0];
+
+        switch (sCommand){
+         case ABOUT:   System.out.println("тестовое приложение"); break;
+         case VERSION: System.out.println("О приложении");break;
+         case HELP:    System.out.println("Помощь" + "\n vesrsion  версия приложения" + "\n about о приложении");break;
+         case DEFAULT: System.out.println("умолчание");break;
+         default:      System.out.println("Нет такой команды");
+        }
+     return;
+
+    }
+
+
+
+
 }
---
+++ src/ru/ovk/home/constant/TerminalConst.java
@@ -0,0 +1,9 @@
+package ru.ovk.home.constant;
+
+public class TerminalConst {
+    public static final String HELP="help";
+    public static final String VERSION="version";
+    public static final String ERROR="error";
+    public static final String ABOUT="about";
+    public static final String DEFAULT="default";
+}
